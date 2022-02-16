240f7675396888c5d1cf52e7d4da5aa05ca2afe6
Bufovsky <39122181+Bufovsky@users.noreply.github.com>
2022-02-16 08:23:08 +0100

    Update login.blade.php
    Reset password update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,14 +62,6 @@
                                 <a class="btn btn-link" href="{{ route('register') }}">
                                         Register
                                     </a> 
-
-                                 | 
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
